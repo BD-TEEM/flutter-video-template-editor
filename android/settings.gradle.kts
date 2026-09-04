2b425cd35e07c98d1f72222577f5faa7577fb816
BD-TEEM <298979698+BD-TEEM@users.noreply.github.com>
2026-09-05 03:36:18 +0600

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -26,12 +26,12 @@ plugins {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
         maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
 }
 
