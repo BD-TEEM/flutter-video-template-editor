5ffc6132906d9ae16b42acedef0c6294b5a957a5
BD-TEEM <298979698+BD-TEEM@users.noreply.github.com>
2026-09-06 06:06:32 +0600

    Update bg_removal_service.dart

--- lib/features/editor/services/bg_removal_service.dart
+++ lib/features/editor/services/bg_removal_service.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new/return_code.dart';
 
 class BgRemovalService {
   /// Green/Single Color background removal using FFmpeg Chromakey
